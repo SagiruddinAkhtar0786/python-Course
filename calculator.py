74d5f40a1d1080515173a5513c10f8b750dbc43e
Sagiruddin Akhtar <kp19nimcet20@gmail.com>
2026-06-13 18:31:02 +0530

    commit python Code

---
+++ calculator.py
@@ -0,0 +1,37 @@
+a = 5
+b = 10
+print("summation of, " + str(a) + " ,and " + str(b) + ", is : ", a+b)
+print("subtraction of, " + str(a) + " ,and " + str(b) + ", is : ", a-b)
+print("multiplication of",a,"*", b, " is :  ", a*b) 
+print("division of",a,"/",b,"is : ",a/b) 
+print("division of",a,"/",b,"is : ",a/b) 
+print("division of",a,"/",b,"is : ",a/b) 
+print("division of",a,"/",b,"is : ",a/b) 
+print("division of",a,"/",b,"is : ",a/b) 
+print("division of",a,"/",b,"is : ",a/b) 
+
+""" k = input("Enter a number: ")
+print("You entered: ", k)
+l = input("Enter another number: ")
+print("You entered: ", l)   
+
+print("summ of",k +"and",l,"is:",int(k)+int(l))
+str = '''He said ,
+i ll you meet me at the park at 5pm, and i said,
+\"I want to eat an apple\"'''
+ """
+
+name="sagiruddin"
+""" print(str)
+print(str[0])
+print(str[0:5]) """
+
+print("lets use for loop to print each character of the string")
+for char in name:
+    print(char)
+print ("lets use for loop with index to print each character of the string")
+for i in range(0, len(name)):
+    print(name[i])
+#print index of each character in the string
+for i in range(0, len(name)):
+    print("index: ", i, "character: ", name[i])
---
+++ main.py
@@ -0,0 +1,14 @@
+
+print("Hello, World!")
+print(12*13)
+#pelase dont remove below lines
+
+""" 
+
+
+jk
+dfshf
+dsjhgksh
+ """
+print("The answer to the Ultimate Question of Life \nThe Universe, and Everything is", 42)
+print("sagir",3,4, sep="***", end="!!!\n")
