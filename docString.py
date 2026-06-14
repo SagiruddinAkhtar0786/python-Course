7b48967f712d75eb591ae0935bf9534c62fe8a1c
Sagiruddin Akhtar <kp19nimcet20@gmail.com>
2026-06-14 23:19:43 +0530

    DocString

---
+++ docString.py
@@ -0,0 +1,9 @@
+def square(number):
+    """Returns the square of a number."""
+    return number * number
+
+result = square(7)
+print("The square of 7 is:", result)
+
+
+print(square.__doc__)  # Output: Returns the square of a number.
