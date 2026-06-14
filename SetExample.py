8d47b165bbc673a0dbebdb48ac1bcb6ce3b859b2
Sagiruddin Akhtar <kp19nimcet20@gmail.com>
2026-06-14 23:32:41 +0530

    sets

---
+++ SetExample.py
@@ -0,0 +1,9 @@
+s ={3,5,3,2,2,6,"fsagit","sagir",True,False,3.14   }
+print(s,end=" ")
+print("\nLength of set:", len(s))
+print(type(s))  
+
+typeof = set() # empty set
+print(type(typeof))
+dict1 = {} #this is not a set, this is a dictionary
+print(type(dict1))
