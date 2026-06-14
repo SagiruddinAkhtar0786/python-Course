f45ce027266c483260a82fce0132ddd4bec786cd
Sagiruddin Akhtar <kp19nimcet20@gmail.com>
2026-06-14 23:44:57 +0530

    Dictonary

---
+++ Dictionary.py
@@ -0,0 +1,19 @@
+dic = {
+    "name": "Alice",
+    "age": 30,
+    "city": "New York",
+    "designation": "Software Engineer"
+}
+
+""" printquote = f"{dic['name']} is a {dic['age']} year old {dic['designation']} living in {dic['city']}."
+print(printquote) """
+
+""" for key, value in dic.items():
+    print(f"{key}: {value}")     """
+
+for key in dic.keys():
+    print(key)
+
+for value in dic.values():
+    print(value)
+
