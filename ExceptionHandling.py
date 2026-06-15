2f0849fa0715e3354dc0631a266654d26c6849de
Sagiruddin Akhtar <kp19nimcet20@gmail.com>
2026-06-15 23:59:33 +0530

    commit

---
+++ ExceptionHandling.py
@@ -0,0 +1,39 @@
+num = int(input("Enter a number: "))
+
+
+""" try:
+    num = int(num)  # Attempt to convert the input to an integer
+    print("You entered:", num) 
+    if num < 0:
+        raise ValueError("Negative numbers are not allowed.")
+except ValueError as e:
+    print("Invalid input! Please enter a valid integer.")
+    print("Error:", e)
+ """
+
+""" 
+try:
+    result = 10 / num  # Attempt to divide by the input number
+    print("Result of division:", result)
+except ZeroDivisionError:
+    print("Error: Division by zero is not allowed.")
+ """
+l = [3,4,5,73,3]
+""" try:
+    for i, value in enumerate(l):
+        print(i, value)
+except Exception as e:
+    print("invalid number",e)
+finally :
+    print("i will be printed always")
+ """
+
+try :
+    for i in range(7):
+        print(l[i+1])
+except Exception as e:
+        print("error : ",e)
+finally:
+    print("always executable")
+
+
