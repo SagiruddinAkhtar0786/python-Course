1d37fd76cf243c5e3b8f1899afae045c7b548d96
Sagiruddin Akhtar <kp19nimcet20@gmail.com>
2026-06-16 00:30:58 +0530

    indexing

--- loop.py
+++ loop.py
@@ -12,3 +12,16 @@ for i in range(5):
 
 else:
     print("Loop has completed.")
+
+
+#indexing in loop  enumurate
+
+
+fruits = ["sagir","rahu","amit","ravi"]
+for index , fruit in enumerate(fruits):
+    print(index,fruit)
+print("\n88888888888888888888888888\n")
+
+#fruits = ["sagir","rahu","amit","ravi"]
+for index , fruit in enumerate(fruits,start=1):
+    print(index,fruit)
